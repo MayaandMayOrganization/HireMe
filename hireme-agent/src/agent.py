fc0422f01ba0c30548826bbe4214a4b1d0da7703
mayageva2 <mayageva7@gmail.com>
2026-05-10 11:31:44 +0300

    moving to ecs

--- hireme-agent/src/agent.py
+++ hireme-agent/src/agent.py
@@ -22,9 +22,6 @@ from livekit.agents import WorkerOptions
 logger = logging.getLogger("agent")
 
 load_dotenv(".env")
-os.environ["AWS_ACCESS_KEY_ID"] = os.getenv("AWS_ACCESS_KEY_ID", "")
-os.environ["AWS_SECRET_ACCESS_KEY"] = os.getenv("AWS_SECRET_ACCESS_KEY", "")
-os.environ["AWS_SESSION_TOKEN"] = os.getenv("AWS_SESSION_TOKEN", "") 
 os.environ["AWS_REGION"] = os.getenv("AWS_REGION", "us-east-1")
 
 class Assistant(Agent):
